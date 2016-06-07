75cbb8a5ba55cc4868cd8e80a00d2e3fe001e69e
Aliaksei Harabchuk <aliaksei.harabchuk@gmail.com>
2016-06-07 12:08:48 +0300

    fixed: empty values prevented from sending an event, added: the debug flag

--- gasender.php
+++ gasender.php
@@ -6,13 +6,17 @@ class GaSender
 {
 	var $trackingId='';
 	var $clientId='';
-	
+	var $debug=false;
+	var $lastError='';
+	var $lastInfo='';
+	var $lastPayload='';
+
 	function __construct($trackingId, $clientId){
 		$this->trackingId=$trackingId;
 		$this->clientId=$clientId;
 	}
-	
-	function event($category, $action, $label='', $value=''){
+
+	function event($category, $action, $label=null, $value=null){
 		return array(
 			't'=>'event',
 			'ec'=>$category,
@@ -24,11 +28,16 @@ class GaSender
 	}
 
 	function send(array $payload){
-		$data = array_merge($payload, array('v'=>1, 'tid'=>$this->trackingId, 'cid'=>$this->clientId));
+		// remove empty values, GA does not accept it
+		$cleanedPayload = array_filter($payload, function($v){
+				return ($v===0 ? true : !empty($v));
+		});
+		$data = array_merge($cleanedPayload, array('v'=>1, 'tid'=>$this->trackingId, 'cid'=>$this->clientId));
 		$url = 'http://www.google-analytics.com/collect';
-		$content = http_build_query($data); 
-		$content = utf8_encode($content); // The payload must be UTF-8 encoded.
-		$user_agent = 'Sipuni GaHelper (Sipuni.com)';
+		$content = http_build_query($data);
+		$content = utf8_encode($content);
+		$user_agent = 'Php GA Tools (metricexpert.com)';
+
 		$ch = curl_init();
 		curl_setopt($ch, CURLOPT_USERAGENT, $user_agent);
 		curl_setopt($ch, CURLOPT_URL, $url);
@@ -36,7 +45,20 @@ class GaSender
 		curl_setopt($ch, CURLOPT_HTTP_VERSION,CURL_HTTP_VERSION_1_1);
 		curl_setopt($ch, CURLOPT_POST, TRUE);
 		curl_setopt($ch, CURLOPT_POSTFIELDS, $content);
-		curl_exec($ch);
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+		$result = curl_exec($ch);
+		$ok = $result!==false;
+
+		if(!$ok){
+			$this->lastError = curl_error($ch);
+		}
+
+		if($this->debug){
+			$this->lastInfo = curl_getinfo($ch);
+			$this->lastPayload = $data;
+		}
+
 		curl_close($ch);
+		return $ok;
 	}
 }
